ab15e8b7e678075406514c914299f7761ddbe532
Art Hakobyan <dr.arthurhakobyan@gmail.com>
2025-08-20 18:03:26 +0400

    Add Day 2 quiz fixes and Day 3 exercises

--- day3.sql
+++ day3.sql
@@ -1,7 +1,16 @@
 -- Day 3 SQL Exercises (ORDER BY & DISTINCT)
 
 -- 1. List all customers ordered by LastName (ascending)
+SELECT FirstName, LastName, Country
+FROM Customer
+ORDER BY LastName ASC;
 
 -- 2. List all invoices ordered by Total (descending)
+SELECT InvoiceId, CustomerId, Total
+FROM Invoice
+ORDER BY Total DESC;
 
 -- 3. Find all distinct billing countries from Invoice
+SELECT DISTINCT BillingCountry
+FROM Invoice
+ORDER BY BillingCountry;

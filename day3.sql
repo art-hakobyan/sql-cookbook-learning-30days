7f2beca9d6c5d5c0225c45e3bddf523d4688637d
Art Hakobyan <dr.arthurhakobyan@gmail.com>
2025-08-22 16:14:26 +0400

    Day 3 exercises

--- day3.sql
+++ day3.sql
@@ -14,3 +14,34 @@ ORDER BY Total DESC;
 SELECT DISTINCT BillingCountry
 FROM Invoice
 ORDER BY BillingCountry;
+
+-- 4. Get the top 10 longest tracks (by Milliseconds)
+SELECT Name, Milliseconds
+FROM Track
+ORDER BY Milliseconds DESC
+LIMIT 10;
+
+-- 5. Find the 5 most expensive invoices (by Total).
+SELECT *
+FROM Invoice
+ORDER BY total DESC
+LIMIT 5;
+
+-- 6. List all customers ordered by Country, then City.
+SELECT *
+FROM Customer
+WHERE State != ''
+ORDER BY Country ASC;
+
+-- 7. Select the 3 shortest tracks, showing Name and Milliseconds.
+SELECT Name, Milliseconds
+FROM Track
+ORDER BY Milliseconds ASC
+LIMIT 3;
+
+
+-- 8. Show invoices from the USA ordered by BillingPostalCode (descending)
+SELECT *
+FROM Invoice
+WHERE BillingCountry LIKE '%USA%'
+ORDER BY BillingPostalCode DESC;

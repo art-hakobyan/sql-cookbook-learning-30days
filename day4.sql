69338f59054bb1ed2344c851833b92614b7147d1
Art Hakobyan <dr.arthurhakobyan@gmail.com>
2025-08-22 16:23:39 +0400

    Add Day 4 template with calculated fields exercises

--- day4.sql
+++ day4.sql
@@ -5,3 +5,29 @@
 -- 2. Show InvoiceId and Total with 20% tax added as "TotalWithTax"
 
 -- 3. Show Customer Name and Email in one concatenated field
+
+-- Day 4 SQL Exercises (Calculated Fields)
+
+-- 1. Show Track Name and length in minutes (Milliseconds / 60000.0)
+SELECT Name,
+       ROUND(Milliseconds / 60000.0, 2) AS LengthMinutes
+FROM Track
+ORDER BY LengthMinutes DESC
+LIMIT 10;
+
+-- 2. Show InvoiceId and Total with 20% tax added as "TotalWithTax"
+SELECT InvoiceId,
+       Total,
+       ROUND(Total * 1.2, 2) AS TotalWithTax
+FROM Invoice
+ORDER BY TotalWithTax DESC
+LIMIT 10;
+
+-- 3. Show Customer Name and Email in one concatenated field
+SELECT FirstName || ' ' || LastName || ' <' || Email || '>' AS ContactInfo
+FROM Customer
+ORDER BY LastName, FirstName;
+
+-- 4. (Optional Challenge) Find the average track length in minutes
+SELECT ROUND(AVG(Milliseconds) / 60000.0, 2) AS AvgTrackLengthMinutes
+FROM Track;
